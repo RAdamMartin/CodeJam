c5a7290dea83f6e484081b48faa1934cc050cfd4
Adam Martin <adam.martin223@gmail.com>
2016-08-08 17:34:59 -0400

    Started jammer wrapper

---
+++ jammer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* agrv[]){
+    if (argc == 2) {
+        
+    } else if (argc == 3) {
+
+    } else {
+        std::cerr << "Invalid number of arguments. Jammer takes 2 arguments; a path to an executable, and a size of each test case."
+    }
+}
