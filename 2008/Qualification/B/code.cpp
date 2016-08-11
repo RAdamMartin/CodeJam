c4ed920e60f5ee4a2d72b08438c8d21d984e707f
Adam Martin <adam.martin223@gmail.com>
2016-08-11 17:27:06 -0400

    finished B

--- 2008/Qualification/B/code.cpp
+++ 2008/Qualification/B/code.cpp
@@ -1,7 +1,8 @@
-
-#include <string>
-#include <set>
+#include <algorithm>
 #include <iostream>
+#include <set>
+#include <sstream>
+#include <string>
 
 class ScheduleManager {
     private:
@@ -12,13 +13,13 @@ class ScheduleManager {
             std::stringstream ss(times);
             uint ret, temp;
             ss >> ret;
-            first *= 60;
+            ret *= 60;
             ss >> temp;
             ret += temp;
             return ret;
-        }
+        };
     public:
-        ScheduleManager(uint turnaround=0) : turnaround_(turnaround)}{};
+        ScheduleManager(uint turnaround=0) : turnaround_(turnaround){};
         void addArrival(std::string time){
             arrivals_.emplace(getMinutes(time) + turnaround_);
         };
@@ -26,14 +27,21 @@ class ScheduleManager {
             departures_.emplace(getMinutes(time));
         };
         uint calculateNumRequired(){
-            return 0;
+            for(const auto &a : arrivals_){
+                auto match = departures_.lower_bound(a);
+                if (match != departures_.end()){
+                    departures_.erase(match);
+                }
+            }
+            return departures_.size();
         };
-}
+};
 
 void processLine(uint numLines, ScheduleManager& departs, ScheduleManager& arrives){
     while(numLines-- > 0){
         std::string temp;
         std::getline(std::cin, temp);
+        std::replace(temp.begin(), temp.end(), ':', ' ');
         departs.addDeparture(temp.substr(0,5));
         arrives.addArrival(temp.substr(5));
     }
@@ -41,18 +49,19 @@ void processLine(uint numLines, ScheduleManager& departs, ScheduleManager& arriv
 
 int main(){ 
     std::string temp;
-    const uint N,T,A,B;
-    std::cin >> N >> T >> A >> B;
-    std::getline(std::cin, temp);
+    uint N,T,A,B;
+    std::cin >> N;
 
     for (uint i = 1; i <= N; i++){ //Iterate through test cases
-        std::cout << "Case #" << i << ": ";
+        std::cin >> T >> A >> B;
+        std::getline(std::cin, temp);
         
         ScheduleManager station_A(T);
         ScheduleManager station_B(T);
         processLine(A, station_A, station_B);
         processLine(B, station_B, station_A);
-        
-        std::cout << station_A << " " << station_B << "\n";
+
+        std::cout << "Case #" << i << ": "        
+                  << station_A.calculateNumRequired() << " " << station_B.calculateNumRequired() << "\n";
     } 
 }
