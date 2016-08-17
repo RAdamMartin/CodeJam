1ca89c77959f557728d11eb61d82491fbc8b7c0f
Adam Martin <adam.martin223@gmail.com>
2016-08-16 22:21:31 -0400

    solved C

--- 2008/Qualification/C/code.cpp
+++ 2008/Qualification/C/code.cpp
@@ -2,65 +2,91 @@
 #include <iostream>
 #include <iomanip>
 
-const double pi = 3.14159265358979323846;
+namespace cst{
+    static constexpr double pi = acos(-1);
+}
 
-double getArea(double chord_rad, double chord_len, double delta){
-    double overlap = chord_rad;
-    bool joint = false;
-    double dist = chord_len/2 - overlap;
-    while(dist > 0){
-        if (joint){
-            dist -= chord_rad*2;
-            overlap += chord_rad*2;
-        } else {
-            dist -= delta;
+double getArea(double x, double y, double g, double rad){
+    auto isOutsideCircle = [rad](double x, double y){
+        return x*x+y*y >= rad*rad;
+    };
+    auto pythag = [rad](double len){
+        return std::sqrt(rad*rad - len*len);
+    };
+
+    if (isOutsideCircle(x, y)){
+        return 0.0d;
+    }
+
+    double x1 = x + g;
+    double y1 = y + g;
+    if (!isOutsideCircle(x1, y1)){
+        return g*g;
+    } 
+
+    double a, b, area, theta;
+    area = 0.0d;
+    if (isOutsideCircle(x, y1)){
+        theta = std::acos(x/rad);
+        if (isOutsideCircle(x1, y)){   //Only bottom left corner inside
+            theta -= std::asin(y/rad);
+            a = pythag(y) - x;
+            b = pythag(x) - y;
+        } else {    //Only bottom half inside
+            theta -= std::acos(x1/rad);
+            a = pythag(x) + pythag(x1) - 2*y;
+            b = g;
         }
-        joint = !joint;
+    } else {
+        theta = std::asin(y1/rad);
+        if (isOutsideCircle(x1, y)){   //Only left half inside
+            theta -= std::asin(y/rad);
+            a = pythag(y) + pythag(y1) - 2*x;
+            b = g;
+        } else {      //All but top right corner inside
+            theta -= std::acos(x1/rad);
+            a = x1 - pythag(y1);
+            b = pythag(x1) - y1;
+            area += g*g;
+        }
     }
-    if (joint){
-        overlap += dist;
-    }
-    overlap /= chord_len;
+    return area + a*b/2 + rad*rad*(theta - std::sin(theta))/2.0d;
+}
 
-    double temp = 2*chord_rad*chord_len;
-    temp *= (1-overlap);
-    return temp;
+double getSolution(double f, double R, double t, double r, double g){
+    t = (t+f)/R;
+    r = (r+f)/R;
+    g = (g-2*f)/R;
+    R = 1.0d-t;
+    if (g <= 0.0d){
+        return 1.0d;
+    }
+
+    double empty = 0.0d;
+    double delta = g+2*r;
+    double x, y;
+    x = r;
+    while (x < R){
+        y = r;
+        while (y < R){
+            empty += getArea(x, y, g, R);
+            y += delta;
+        }
+        x += delta;
+    }
+    return 1.0d - 4.0d*empty/cst::pi;
 }
 
 int main(){ 
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
+    std::cout << std::setprecision(6) << std::fixed;
+
     uint N;
-    double f,R,t,r,g;
     std::cin >> N;
-    std::cout << std::setprecision(6) << std::fixed ;
     for (uint i = 1; i <= N; i++){ //Iterate through test cases
+        double f,R,t,r,g;
         std::cin >> f >> R >> t >> r >> g;
-        
-        if (g > 2*f) {
-            double inner_rad = R-t-f;
-
-            double chord_delta = g+2*r;
-            double chord_len = 2*inner_rad;
-            double chord_rad = r+f;
-
-            double numerator = pi*(R*R-inner_rad*inner_rad);
-            double denominator = pi*R*R;
-
-            double pos = 0;
-            while(pos+chord_rad < inner_rad){
-                numerator += getArea(chord_rad, chord_len, g - 2*f)* (pos == 0 ? 2 : 4);
-                pos += chord_delta;
-                chord_len -= chord_delta*chord_delta/chord_len;
-            }
-            double new_rad = inner_rad-pos;
-            chord_len += (2*chord_delta*new_rad-new_rad*new_rad)/chord_len;
-            numerator += getArea(new_rad, chord_len, g - 2*f)* (pos == 0 ? 2 : 4);
-
-            
-            std::cout << "Case #" << i << ": " << numerator/denominator << "\n";
-        } else {
-            std::cout << "Case #" << i << ": " << 1.0f << "\n";
-        }
+        std::cout << "Case #" << i << ": " << getSolution(f,R,t,r,g) << "\n";
     } 
 }
